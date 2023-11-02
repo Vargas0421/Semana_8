11c7af1e0d4f59e733ac163ab453ea07fef13b8d
Vargas0421 <136260759+Vargas0421@users.noreply.github.com>
2023-11-01 18:35:03 -0600

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calse1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LABORATORIO 04
+ */
+public class Calse1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        int edad[] = new int [3]; // decalracion de la variable 
+        for (int i = 0; i < edad.length; i++) {
+            edad[i] = Integer.parseInt(JOptionPane.showInputDialog("digite la edad"));
+        }
+         for (int i = 0; i < edad.length; i++) {
+            JOptionPane.showMessageDialog(null, edad[i]);
+        }
+    }
+    
+}
